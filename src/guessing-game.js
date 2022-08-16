baf784fbf9e50533af932a64e744fb32c3277178
olegshulgin <97721233+olegshulgin@users.noreply.github.com>
2022-08-16 17:39:16 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.guessing = null;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      return this.guessing = this.min + Math.round((this.max - this.min) / 2);
     }
 
     lower() {
-
+      this.max = this.guessing;
     }
 
     greater() {
-
+      this.min = this.guessing;
     }
 }
 
